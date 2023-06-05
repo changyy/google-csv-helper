75d53b420e5f5593351dbd2d7a8261c9226946f3
Yuan-Yi Chang <changyy.csie@gmail.com>
2023-06-05 23:49:25 +0800

    update setup.py usage

--- google_csv_helper/__init__.py
+++ google_csv_helper/__init__.py
@@ -8,4 +8,4 @@ from google_csv_helper import csv_common
 from google_csv_helper import csv_helper
 from google_csv_helper import adsense_csv_helper
 
-version = '1.0.3'
+version = '1.0.4'
--- setup.py
+++ setup.py
@@ -20,6 +20,17 @@ URL = "https://github.com/changyy/google-csv-helper"
 DOWNLOAD_URL = "https://pypi.org/project/google-csv-helper/"
 DESCRIPTION = 'A simple tool that takes CSV reports from Google Adsense, Google Admob, and Google Analytics and outputs them in JSON / Pandas.DataFrame format.'
 LONG_DESCRIPTION = DESCRIPTION
+LONG_DESCRIPTION_TYPE = 'text/plain'
+try:
+    with open(os.path.join(CWD, "README.md"), 'r') as f:
+        data = f.read()
+        if len(data) > 10:
+            LONG_DESCRIPTION = data
+            LONG_DESCRIPTION_TYPE = 'text/markdown'
+except Exception as e:
+    pass
+
+
 INSTALL_REQUIRES = ['pandas']
 try:
     with open(os.path.join(CWD, "requirements.txt"), 'r') as f:
@@ -34,6 +45,7 @@ setup(
     author_email="<changyy.csie@gmail.com>",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESCRIPTION_TYPE,
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
     keywords=['python', 'google', 'csv', 'adsense', 'admob', 'report'],
